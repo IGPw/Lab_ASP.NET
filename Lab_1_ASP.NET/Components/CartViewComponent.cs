16178156de73f2b7585fb79f97f48e2ca9ca6ab2
IGPw <igor.potapovw@gmail.com>
2023-11-22 23:14:44 +0300

    Добавьте файлы проекта.

---
+++ Lab_1_ASP.NET/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_1_ASP.NET.Components {
+    public class CartViewComponent : ViewComponent {
+        public CartViewComponent() {
+
+        }
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
---
+++ Lab_1_ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Lab_1_ASP.NET.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
